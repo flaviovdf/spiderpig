7a6e3f2fc14886aa6f52653de4acd389b5a424e1
Flavio Figueiredo <flaviovdf@gmail.com>
2012-10-06 11:35:28 -0300

    Removed jobs to new repo

--- src/br/ufmg/dcc/vod/ncrawler/jobs/youtube/user_api/YTErrorPageException.java
+++
@@ -1,5 +0,0 @@
-package br.ufmg.dcc.vod.ncrawler.jobs.youtube.user_api;
-
-public class YTErrorPageException extends Exception {
-
-}
--- src/br/ufmg/dcc/vod/ncrawler/ui/CrawlerPool.java
+++ src/br/ufmg/dcc/vod/ncrawler/ui/CrawlerPool.java
@@ -4,29 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import br.ufmg.dcc.vod.ncrawler.evaluator.EvaluatorFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.lastfm.artistsongs_api.ArtistSongFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.lastfm.song_cats.LastFMCatsEvalFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.lastfm.user_apihtml.LFMApiFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.plus.PlusFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.urlsaver.URLDownFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.youtube.html.YTHtmlStatsFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.youtube.user_api.YTApiFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.youtube.video_api.YTVideoApiFactory;
-import br.ufmg.dcc.vod.ncrawler.jobs.youtube.videoresp_api.YoutubeResponseFactory;
 
 public class CrawlerPool {
 
 	private static final Map<String, EvaluatorFactory<?,?,?>> crawlers = new HashMap<String, EvaluatorFactory<?,?,?>>();
 	static {
-		crawlers.put("YTAPI", new YTApiFactory());
-		crawlers.put("YTVIDAPI", new YTVideoApiFactory());
-		crawlers.put("LFM", new LFMApiFactory());
-		crawlers.put("LFM_MUSIC", new ArtistSongFactory());
-		crawlers.put("YOUTUBE_RESPONSE", new YoutubeResponseFactory());
-		crawlers.put("LFM_CATS", new LastFMCatsEvalFactory());
-		crawlers.put("YTHTMLSTATS", new YTHtmlStatsFactory());
-		crawlers.put("GPLUS", new PlusFactory());
-		crawlers.put("URL", new URLDownFactory());
+		;
+//		crawlers.put("YTAPI", new YTApiFactory());
+//		crawlers.put("YTVIDAPI", new YTVideoApiFactory());
+//		crawlers.put("LFM", new LFMApiFactory());
+//		crawlers.put("LFM_MUSIC", new ArtistSongFactory());
+//		crawlers.put("YOUTUBE_RESPONSE", new YoutubeResponseFactory());
+//		crawlers.put("LFM_CATS", new LastFMCatsEvalFactory());
+//		crawlers.put("YTHTMLSTATS", new YTHtmlStatsFactory());
+//		crawlers.put("GPLUS", new PlusFactory());
+//		crawlers.put("URL", new URLDownFactory());
 	}
 	
 	public static EvaluatorFactory<?,?,?> get(String name) {
