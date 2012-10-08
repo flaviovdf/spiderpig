c6d33e0b3ff0ccc776c0ab33b64952b0c393a664
Flavio Figueiredo <flaviovdf@gmail.com>
2012-10-07 23:20:51 -0300

    Removed imports from sun. Copyright warnings gone

--- src/br/ufmg/dcc/vod/ncrawler/distributed/rmi/AbstractRMIFactory.java
+++ src/br/ufmg/dcc/vod/ncrawler/distributed/rmi/AbstractRMIFactory.java
@@ -5,37 +5,74 @@ import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-import sun.rmi.registry.RegistryImpl;
-
+/**
+ * Helper object to create and Bind RMI objects.
+ * 
+ * @author Flavio Figueiredo - flaviovdf 'at' gmail.com
+ *
+ * @param <T> Type f object to create
+ */
 public abstract class AbstractRMIFactory<T extends Remote> {
 
 	private final int port;
 	private Registry ri;
 	private T create;
 
+	/**
+	 * Creates a new Factory which will bind objects at the RMI registry in the
+	 * given port.
+	 * 
+	 * @param port Port to use
+	 * @throws RemoteException If registry cannot be created / located.
+	 */
 	public AbstractRMIFactory(int port) throws RemoteException {
 		this.port = port;
-		this.ri = new RegistryImpl(port);
+		this.ri = LocateRegistry.createRegistry(port);
 	}
 	
-	public final T createAndBind() throws RemoteException, AlreadyBoundException {
+	/**
+	 * Create and Bind the object
+	 * 
+	 * @return The created Remote object
+	 * 
+	 * @throws RemoteException If cannot bind
+	 * @throws AlreadyBoundException If cannot bind
+	 */
+	public final T createAndBind() 
+			throws RemoteException, AlreadyBoundException {
+		
 		this.create = create(port);
-		ri.bind(getName(), create);
+		this.ri.bind(getName(), create);
 		return create;
 	}
 
-	public final void shutdown() throws AccessException, RemoteException, NotBoundException {
-		ri.unbind(getName());
-		UnicastRemoteObject.unexportObject(create, true);
-		UnicastRemoteObject.unexportObject(ri, true);
-		create = null;
-		ri = null;
+	public final void shutdown() 
+			throws AccessException, RemoteException, NotBoundException {
+		
+		this.ri.unbind(getName());
+		UnicastRemoteObject.unexportObject(this.create, true);
+		UnicastRemoteObject.unexportObject(this.ri, true);
+		this.create = null;
+		this.ri = null;
 	}
 
+	/**
+	 * Return the name which the object will be bound to
+	 * 
+	 * @return Name to bind
+	 */
 	public abstract String getName();
 	
+	/**
+	 * Creates the remote object
+	 * 
+	 * @param port
+	 * @return
+	 * @throws RemoteException
+	 */
 	public abstract T create(int port) throws RemoteException;
 }
