aecc428529c0b931433fd94b023762f57f0dec10
Flavio Figueiredo <flaviovdf@gmail.com>
2012-10-29 14:55:18 -0200

    Added options

--- spiderpig.sh
+++ spiderpig.sh
@@ -8,7 +8,7 @@ findself
 shopt -s huponexit #Makes sure doesn't get killed on exit, usefull for ssh
 
 cd $SELF
-    java -jar vod-crawler.jar
+    java -jar vod-crawler.jar $*
     excode=$?
 cd  > /dev/null
 
