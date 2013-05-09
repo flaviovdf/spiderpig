633670bba4b590fcd361151a330401336dce644d
Flavio Figueiredo <flaviovdf@gmail.com>
2013-05-09 08:06:27 -0300

    Added comments

--- src/br/ufmg/dcc/vod/spiderpig/common/URLGetter.java
+++ src/br/ufmg/dcc/vod/spiderpig/common/URLGetter.java
@@ -1,6 +1,7 @@
 package br.ufmg.dcc.vod.spiderpig.common;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -9,6 +10,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * Converts URL's to byte arrays. Can also set some requests properties
+ * such as encoding and language. Useful for crawling sites with locale
+ * settings. 
+ * 
+ * @author Flavio Figueiredo - flaviovdf 'at' gmail.com
+ */
 public class URLGetter {
 
 	public static final String UTF8 = "utf-8";
@@ -18,6 +26,10 @@ public class URLGetter {
 	private Map<String, String> requestProperties;
 	private Charset charSet;
 	
+	/**
+	 * Constructs URL which will send requests with default charset (utf8) and
+	 * accepting English languages.
+	 */
 	public URLGetter() {
 		this.requestProperties = new HashMap<>();
 		this.requestProperties.put("Accept-Charset", UTF8);
@@ -25,6 +37,12 @@ public class URLGetter {
         this.charSet = Charset.forName(UTF8);
 	}
 
+	/**
+	 * Constructs URL getter getter for the given charset and language.
+	 * 
+	 * @param charset Charset abbreviation
+	 * @param lang Language abbreviation, if multiple separated by comma
+	 */
 	public URLGetter(String charset, String lang) {
 		this.requestProperties = new HashMap<>();
 		this.requestProperties.put("Accept-Charset", charset);
@@ -32,8 +50,19 @@ public class URLGetter {
 		this.charSet = Charset.forName(charset);
 	}
 	
+	/**
+	 * Converts URL page to byte array
+	 * 
+	 * @param u URL to connect
+	 * @param header Header to add to byte array
+	 * @param footer Footer to add to byte array
+	 * 
+	 * @return byte array
+	 * 
+	 * @throws IOException If unable to connect to URL 
+	 */
 	public byte[] getHtml(URL u, String header, String footer) 
-			throws Exception {
+			throws IOException {
 		BufferedReader in = null;
 		try {
 			URLConnection openConnection = u.openConnection();
