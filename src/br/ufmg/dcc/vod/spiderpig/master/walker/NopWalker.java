db2e3055214468ae83715a0f2e3b17b78560f22b
Flavio Figueiredo <flaviovdf@gmail.com>
2013-05-25 15:02:29 -0300

    Added no operation walker

---
+++ src/br/ufmg/dcc/vod/spiderpig/master/walker/NopWalker.java
@@ -0,0 +1,53 @@
+package br.ufmg.dcc.vod.spiderpig.master.walker;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.configuration.Configuration;
+
+import br.ufmg.dcc.vod.spiderpig.common.config.AbstractConfigurable;
+import br.ufmg.dcc.vod.spiderpig.master.walker.monitor.ExhaustCondition;
+import br.ufmg.dcc.vod.spiderpig.master.walker.monitor.StopCondition;
+import br.ufmg.dcc.vod.spiderpig.protocol_buffers.Ids.CrawlID;
+
+/**
+ * A walker which follows no links. Useful for when crawling just a static set
+ * of pages.
+ * 
+ * @author Flavio Figueiredo - flaviovdf 'at' gmail.com
+ */
+public class NopWalker extends AbstractConfigurable<Void> 
+		implements ConfigurableWalker {
+
+	private final StopCondition stopCondition = new ExhaustCondition();
+	
+	@Override
+	public List<CrawlID> getToWalk(CrawlID crawled, List<CrawlID> links) {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public void addSeedID(CrawlID seed) {
+	}
+
+	@Override
+	public List<CrawlID> getSeedDispatch() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public StopCondition getStopCondition() {
+		return stopCondition;
+	}
+
+	@Override
+	public Set<String> getRequiredParameters() {
+		return Collections.emptySet();
+	}
+
+	@Override
+	public Void realConfigurate(Configuration configuration) throws Exception {
+		return null;
+	}
+}
