2cf32215087249b4fbb92a2a929ee42ee1d52a7f
Flavio Figueiredo <flaviovdf@gmail.com>
2013-06-22 23:18:56 -0300

    Added twitter crawler

---
+++ src/br/ufmg/dcc/vod/spiderpig/jobs/twitter/TwitterSearchRequester.java
@@ -0,0 +1,38 @@
+package br.ufmg.dcc.vod.spiderpig.jobs.twitter;
+
+import java.util.List;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import br.ufmg.dcc.vod.spiderpig.jobs.Requester;
+
+public class TwitterSearchRequester implements Requester<byte[]> {
+
+	private final Twitter twitter;
+
+	public TwitterSearchRequester(Twitter twitter) {
+		this.twitter = twitter;
+	}
+	
+	@Override
+	public byte[] performRequest(String crawlID) throws Exception {
+		Query query = new Query(crawlID);
+		QueryResult result;
+		StringBuilder returnValue = new StringBuilder();
+		do {
+			result = twitter.search(query);
+			List<Status> tweets = result.getTweets();
+			int i = 0;
+			for (Status tweet : tweets) {
+				returnValue.append(i++);
+				returnValue.append(" - ");
+				returnValue.append(tweet);
+				returnValue.append(System.lineSeparator());
+			}
+		} while ((query = result.nextQuery()) != null);
+		
+		return returnValue.toString().getBytes();
+	}
+}
